352ea048d6ffd2152c18f48e775b7ba4ee25aa08
Adam <adam@mac.attlocal.net>
2019-09-10 07:50:22 -0500

    Modified index.php file
    -Index.php now only has one include_once command
    -html and body tags deleted

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php 
-echo "<html><body>";
-
-
-
-include_once("/html/GreatDeveloper66/index.html"); 
-include_once("/html/VarunR/index.html");
-
-echo "</html></body>";
+include_once("index.html");
 ?>
 
 
