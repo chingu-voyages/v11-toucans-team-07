842bd243f9db12dc537b1f60a9c63e3761b8ffcf
Adam <adam@mac.attlocal.net>
2019-10-11 16:33:20 -0500

    Move popup opacity code from index.js to separate file for import

---
+++ js/GreatDeveloper66/scroll.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6*/
+/*global document*/
+export default function popups() {
+  window.onscroll = () => {
+    if (window.scrollY > 800) {
+      document.getElementById("card1VR").style.opacity = 1.0;
+      document.getElementById("card2VR").style.opacity = 1.0;
+      document.getElementById("card3VR").style.opacity = 1.0;
+    }
+    if (window.scrollY > 200) {
+      document.querySelector(".topArrowVR").style.opacity = 1.0;
+    } else {
+      document.querySelector(".topArrowVR").style.opacity = 0.0;
+    }
+  }
+
+
+}
--- js/index.js
+++ js/index.js
@@ -2,18 +2,13 @@
 /*global document*/
 
 import tickyButtonEffects from "../js/GreatDeveloper66/tickyButtonEffects.js";
-tickyButtonEffects();
-window.onscroll = () => {
-    if(window.scrollY > 800){
-      document.getElementById("card1VR").style.opacity = 1.0;
-      document.getElementById("card2VR").style.opacity = 1.0;
-      document.getElementById("card3VR").style.opacity = 1.0;
-    }
-  if(window.scrollY > 200){
-    document.querySelector(".topArrowVR").style.opacity = 1.0;
-  }
-  else {
-    document.querySelector(".topArrowVR").style.opacity = 0.0;
-  }
-  }
+import popups from "../js/GreatDeveloper66/scroll.js";
+
+window.onload = () => {
+  tickyButtonEffects();
+  popups();
+};
+
+
+
 
