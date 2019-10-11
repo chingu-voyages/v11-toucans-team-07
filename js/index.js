c45a128fcbcfb90438314e5fd29b4b4278595f48
Adam <adam@mac.attlocal.net>
2019-10-11 18:33:39 -0500

    Reformat js code to meet JS standards to pass npm test

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
-/*jshint esversion: 6*/
-/*global document*/
-/*global window*/
-/*global CSS*/
+/* jshint esversion: 6 */
+/* global document */
+/* global CSS */
 
-import tickyButtonEffects from "../js/GreatDeveloper66/tickyButtonEffects.js";
-import popups from "../js/GreatDeveloper66/popups.js";
-import scrollEffects from "../js/GreatDeveloper66/scrollEffects.js";
+import tickyButtonEffects from '../js/GreatDeveloper66/tickyButtonEffects.js'
+import popups from '../js/GreatDeveloper66/popups.js'
+import scrollEffects from '../js/GreatDeveloper66/scrollEffects.js'
 
 window.onload = () => {
-  tickyButtonEffects();
-  popups();
-  if (!CSS.supports("scroll-behavior: smooth")) {
-    scrollEffects([...Array.from(document.querySelectorAll("nav a")), document.getElementById("topArrow")]);
+  tickyButtonEffects()
+  popups()
+  if (!CSS.supports('scroll-behavior: smooth')) {
+    scrollEffects([...Array.from(document.querySelectorAll('nav a')), document.getElementById('topArrow')])
   }
-};
+}
